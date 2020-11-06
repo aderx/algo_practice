8945224d6667bddb3fadc36f2fb08fb4bf61624e
FU <aderx@qq.com>
2020-11-06 22:52:15 +0800

    有效括号-代码修正

--- 2020-11/第一周/20-有效的括号/xfu.ts
+++ 2020-11/第一周/20-有效的括号/xfu.ts
@@ -7,21 +7,61 @@
 // 左括号必须以正确的顺序闭合。
 // 注意空字符串可被认为是有效字符串。
 
+// ============================================================
+// ===                     自己想的代码                      ===
+// ===      状态：通过,执行用时: 100 ms,内存消耗: 40.5 MB     ===
+// ============================================================
+
 function isValid(s: string): boolean {
     let stack:string[] = [];
     for(let i=0;i<s.length;i++){
         let stackTop = stack.pop();
         let pareChar = s[i];
-        // 当栈中没有数据时，直接push新值
-        // 当栈中有数据时，若插入的是左括号，则直接插入
-        // 当栈中有数据时，若插入的是右括号则判断是否和出栈的第一个值相匹配，
         // 若不匹配则表示已经有一个不匹配的括号，匹配结束
+        // 当栈中没有数据，或需要进行判断的数据为左括号时，就保存需要存入的值
         if(!stackTop || pareChar === '(' || pareChar === '[' || pareChar === '{'){
             stackTop && stack.push(stackTop);
             stack.push(pareChar);
-        }else if(stackTop === '(' && pareChar !== ')' || stackTop === '[' && pareChar !== ']' ||stackTop === '{' && pareChar !== '}'){
+            // 当栈中取出的值为右括号，则栈不可能被清空了，就直接返回false
+            // 当栈中取出的是左括号,但是匹配的内容不是对应的右括号,则这个左括号无法被消去,则需直接返回false
+        }else if(stackTop === '}' || stackTop === ')' || stackTop === ']' || 
+        stackTop === '(' && pareChar !== ')' || stackTop === '[' && pareChar !== ']' ||stackTop === '{' && pareChar !== '}'){
             return false;
         }
     }
     return !stack.pop()
 };
+
+
+// ============================================================
+// ===                     他人优秀代码                      ===
+// ===      状态：通过,执行用时: 88 ms,内存消耗: 39.5 MB      ===
+// ============================================================
+
+
+let map: any = {
+    '{' : '}',
+    '(' : ')',
+    '[' : ']'
+}
+
+function isValid_TOP(s: string): boolean {
+
+    let stack: string[] = [];
+    let top: string | undefined;
+
+    for(let char of s){
+        let value;
+        if((value = map[char])){
+            stack.push(value);
+        }else{
+            top = stack.pop();
+            if(top !== char){
+                return false;
+            }
+        }
+    }
+
+    return !stack.length;
+}
+
